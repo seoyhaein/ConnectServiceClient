84b618dc32b28e1b10cf82136137462f2204eb2f
HeaInSeo <jwsnsl@knou.ac.kr>
2024-06-22 16:15:17 +0900

    초기 커밋.

---
+++ ConnectServiceClient/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ConnectServiceClient
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
